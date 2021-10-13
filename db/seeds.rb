ab51c07e646e3a2416a853af17d04fa60d9af846
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-13 12:32:19 -0400

    Creates variables with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Bottle", price: 15.00, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpizzalandhalifax.ca%2Fproduct%2Fwater-bottle%2F&psig=AOvVaw3HZ18Zhz2lmpNEA44am8cQ&ust=1634228811388000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOjKyvXmx_MCFQAAAAAdAAAAABAH", description: "A useful plastic waterbottle for when you do anything and want to stay hydrated")
