16d4138d4cffba6e48dd5df1f6634ee1d49bdc61
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-13 12:51:11 -0400

    Creates a method to print all products and then individual products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/all_products" => "products#all_products"
+  get "/first_product" => "products#first_product"
+  get "/second_product" => "products#second_product"
+  get "/third_product" => "products#third_product"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Bottle", price: 15.00, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpizzalandhalifax.ca%2Fproduct%2Fwater-bottle%2F&psig=AOvVaw3HZ18Zhz2lmpNEA44am8cQ&ust=1634228811388000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCOjKyvXmx_MCFQAAAAAdAAAAABAH", description: "A useful plastic waterbottle for when you do anything and want to stay hydrated.")
+Product.create(name: "Bottle", price: 15.00, image_url: "https://pizzalandhalifax.ca/wp-content/uploads/2016/09/45.jpg", description: "A useful plastic waterbottle for when you do anything and want to stay hydrated.")
 
-Product.create(name: "Cow", price: 200.00, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDairy_cattle&psig=AOvVaw3Nb5mRZBgOHlBgdxIeGDzF&ust=1634229215779000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCJiKxLHox_MCFQAAAAAdAAAAABAD", description: "For all ye introverts, to never have to leave the house to buy milk again!")
+Product.create(name: "Cow", price: 200.00, image_url: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg", description: "For all ye introverts, to never have to leave the house to buy milk again!")
 
-Product.create(name: "Cheese", price: 3.00, image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2012%2F06%2F03%2Fmagazine%2Fwho-made-that-kraft-single.html&psig=AOvVaw1h-Trvl3DiNzLjzpZMAZnq&ust=1634229261728000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCMC9ssfox_MCFQAAAAAdAAAAABAD", description: "A slice of cheese.")
+Product.create(name: "Cheese", price: 3.00, image_url: "https://static01.nyt.com/images/2012/06/03/magazine/03wmt/mag-03WMT-t_CA0-superJumbo.jpg", description: "A slice of cheese.")
