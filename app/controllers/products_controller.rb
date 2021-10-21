32a212a8d3bbc02e4b4e497bf5139b01910cea3e
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-21 14:43:51 -0400

    Adds CRUD to suppliers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,6 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(name: params["name"], price: params["price"], image_url: params["image_url"], description: params["description"])
-    product.save
 
     if product.save
       render json: product
