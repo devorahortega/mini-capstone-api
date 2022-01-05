984d9cf72e415a302d3f7d38651714036f798f3a
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-04 19:16:45 -0500

    Updates app

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+  has_many :category_products
+  belongs_to :supplier
   has_many :orders
+
   #validates :name, presence: true, uniqueness: true
   #validates :price, presence: true, numericality: {greater_than: 0}
   #validates :description, length: { minimum: 10, maximum: 100 }
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   current_user
-  attributes :id, :name, :price, :description, :is_discounted?, :tax, :total, :supplier, :images
+  attributes :id, :name, :price, :description, :is_discounted?, :tax, :total, :supplier, :images, :categories
+
+  belongs_to :supplier
+  has_many :categories
 end
